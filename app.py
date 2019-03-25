d9d8880cff6ffc56bd20ae0333f521c654e1321b
Roberto Sanchez <rg.sanchez.a@gmail.com>
2019-03-25 15:40:05 -0500

    Starting all

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
+""""
+      Created by Roberto Sánchez A, based on the Master Thesis:
+    "A proposed method for unsupervised anomaly detection for a multivariate building dataset "
+    University of Bern/Neutchatel/Fribourg - 2017
+    Any copy of this code should be notified at rg.sanchez.a@gmail.com; you can redistribute it
+    and/or modify it under the terms of the MIT License.
+
+    The F.R.E.D.A project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    MIT license terms for more details.
+
+    "My work is well done to honor God at any time" R Sanchez A.
+    Mateo 6:33
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
